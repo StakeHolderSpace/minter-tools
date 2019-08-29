d6d9889a6c45d403bc2febb96813f1899fb6a44a
Evgeniy.Taburyanskiy <Evgeniy.Taburyanskiy@gmail.com>
2019-08-29 15:53:41 +0300

    restore script

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -5,7 +5,7 @@
 
 DEBUG=false
 
-BACKUP_SSH_HOST=minter@94.130.160.69
+BACKUP_SSH_HOST=minter@192.168.1.1
 BACKUP_SSH_PORT=55422
 
 MINTER_HOME=/home/minter
@@ -16,11 +16,16 @@ BACKUP_MINTER_DATA=${BACKUP_SSH_HOST}:${MINTER_HOME}/minter_backups/
 
 
 [[ "$DEBUG" == "true" ]] && echo "====== Restore started ====="
+
+#
+[[ -d  ${MINTER_DATA}/data/ ]] || mkdir -p ${MINTER_DATA}/data/
+[[ -d  ${MINTER_DATA}/tmdata/ ]] || mkdir -p ${MINTER_DATA}/tmdata/
 #
 sudo systemctl stop  ${MINTER_SERVICE_NAME}  &&
 rsync -chavzP -e "ssh -p ${BACKUP_SSH_PORT}" ${BACKUP_MINTER_DATA}/data/ ${MINTER_DATA}/data/&&
-rsync -chavzP -e "ssh -p ${BACKUP_SSH_PORT}" ${BACKUP_MINTER_DATA}/tmdata/  ${MINTER_DATA}/tmdata/ &&
-sudo systemctl start  ${MINTER_SERVICE_NAME}
+rsync -chavzP -e "ssh -p ${BACKUP_SSH_PORT}" ${BACKUP_MINTER_DATA}/tmdata/  ${MINTER_DATA}/tmdata/
+#
+echo "Do shell command if need: 'sudo systemctl restart  ${MINTER_SERVICE_NAME}'"
 #
 [[ "$DEBUG" == "true" ]] && echo "====== Restore finished ====="
 
