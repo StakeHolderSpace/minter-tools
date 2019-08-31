329152fc6a943289518c996d48537d6de7e45f3e
Evgeniy.Taburyanskiy <Evgeniy.Taburyanskiy@gmail.com>
2019-08-31 18:30:05 +0300

    backup рефакторинг

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,12 +9,10 @@ MINTER_HOME=/home/minter
 MINTER_SERVICE_NAME=minter-node
 MINTER_DATA=${MINTER_HOME}/.minter
 BACKUP_ROOT=${MINTER_HOME}/minter_backups
-MINTER_HAS_ERRORS=true
+MINTER_HAS_ERRORS=false
 
 # 0 code - errors not found!
-sudo journalctl -u ${MINTER_SERVICE_NAME}  -n 500 | grep panic > /dev/null
-
-[[ $? -eq 0 ]] && MINTER_HAS_ERRORS=false
+sudo journalctl -u ${MINTER_SERVICE_NAME}  -n 500 | grep -q panic &&  MINTER_HAS_ERRORS=true
 
 [[ "$DEBUG" == "true" ]] && echo "MINTER_HAS_ERRORS=$MINTER_HAS_ERRORS"
 
@@ -26,7 +24,7 @@ if  [[ "$MINTER_HAS_ERRORS" == "false" ]]; then
   [[ -d  ${BACKUP_ROOT}/config/ ]] && rm -rf ${BACKUP_ROOT}/config/
   #
   sudo systemctl stop  ${MINTER_SERVICE_NAME} &&
-  rsync -av --delete ${MINTER_DATA}/ ${BACKUP_ROOT}/ &&
+  rsync -av --delete --exclude 'config/' ${MINTER_DATA}/ ${BACKUP_ROOT}/ &&
   rm -rf ${BACKUP_ROOT}/config/ &&
   sudo systemctl start  ${MINTER_SERVICE_NAME}
   #
