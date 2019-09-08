d07fc35cae77a556ee420e05bc5f82a8678701cc
Evgeniy.Taburyanskiy <Evgeniy.Taburyanskiy@gmail.com>
2019-09-09 01:14:34 +0300

    local ssh restore + backup

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -5,8 +5,9 @@
 
 DEBUG=false
 
-BACKUP_SSH_HOST=minter@192.168.1.1
+BACKUP_SSH_HOST=minter@127.0.0.1
 BACKUP_SSH_PORT=55422
+BACKUP_SSH_PRIVKEY_PATH=~/.ssh/id_rsa_minter
 
 MINTER_HOME=/home/minter
 MINTER_SERVICE_NAME=minter-node
@@ -22,13 +23,12 @@ BACKUP_MINTER_DATA=${BACKUP_SSH_HOST}:${MINTER_HOME}/minter_backups
 [[ -d  ${MINTER_DATA}/tmdata/ ]] || mkdir -p ${MINTER_DATA}/tmdata/
 #
 sudo systemctl stop  ${MINTER_SERVICE_NAME}  &&
-rsync -chavzP -e "ssh -p ${BACKUP_SSH_PORT}" ${BACKUP_MINTER_DATA}/data/ ${MINTER_DATA}/data/ &&
-rsync -chavzP -e "ssh -p ${BACKUP_SSH_PORT}" ${BACKUP_MINTER_DATA}/tmdata/  ${MINTER_DATA}/tmdata/ &&
+rsync -chavzP --progress -e "ssh -p ${BACKUP_SSH_PORT} -i ${BACKUP_SSH_PRIVKEY_PATH}" --recursive ${BACKUP_MINTER_DATA}/ ${MINTER_DATA}/ &&
 chown -R minter:minter ${MINTER_DATA}
 #
 echo "###########################################################################"
 echo ""
-echo "Do shell command if need: 'sudo systemctl restart  ${MINTER_SERVICE_NAME}'"
+echo " Do shell command if need: 'sudo systemctl restart  ${MINTER_SERVICE_NAME}'"
 echo ""
 echo "###########################################################################"
 #
